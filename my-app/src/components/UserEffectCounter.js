6337ad942930344623582aaef47337c9c26c35d0
Thiago Souza <mistersouza@icloud.com>
2024-01-03 16:23:00 +0000

    useEffect to update only when componet did mount

--- my-app/src/components/UserEffectCounter.js
+++ my-app/src/components/UserEffectCounter.js
@@ -2,13 +2,23 @@ import React, { useState, useEffect } from 'react'
 
 function UserEffectCounter() {
     const [ count, setCount ] = useState(0);
+    const [ time, setTime ] = useState(0); 
     
     useEffect(() => {
         document.title = count
     }, [count])
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(time => time + 1)
+        }, 1000)
+    }, [])
+
     return (
-    <button onClick={() => setCount(count => count + 1)}>{count}</button>
+        <div>
+            <button onClick={() => setCount(count => count + 1)}>{count}</button>
+            <h2>{time}</h2>
+        </div>
   )
 }
 
